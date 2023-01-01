c0f33b2601f2c08b09c2ba41698a3f301d27b5d8
Koji Onishi <fursich0@gmail.com>
2023-01-02 00:22:27 +0900

    implement builder method as template

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,12 +1,43 @@
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+    let ident = Ident::new(&format!("{}Builder", name.to_string()), Span::call_site());
+
+    let data = input.data;
+    let fields = match data {
+        Data::Struct(data) => match data.fields {
+            Fields::Named(_) => data.fields,
+            Fields::Unnamed(_) | Fields::Unit => unimplemented!(),
+        },
+        Data::Enum(_) | Data::Union(_) => unimplemented!(),
+    };
+    let field_idents = fields
+        .iter()
+        .map(|field| field.ident.to_owned())
+        .collect::<Vec<_>>();
+    let field_types = fields
+        .iter()
+        .map(|field| field.ty.to_owned())
+        .collect::<Vec<_>>();
+
     let expanded = quote! {
-        // something
+        pub struct #ident {
+            #(#field_idents: Option<#field_types>,)*
+        }
+        impl #name {
+            pub fn builder() -> #ident {
+                #ident {
+                    #(#field_idents: None,)*
+                }
+            }
+        }
     };
 
     TokenStream::from(expanded)
