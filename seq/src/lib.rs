a8739c9ea9ee469bb105ae41d8575643baeed076
Koji Onishi <fursich0@gmail.com>
2023-01-07 23:02:02 +0900

    implement basic parser for seq macro

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,32 @@
 use proc_macro::TokenStream;
+// use quote::quote;
+use syn::parse::{Parse, ParseStream};
+use syn::{parse_macro_input, Block, LitInt, Result, Token};
+
+#[derive(Debug)]
+struct SecMarcoInput {/* ... */}
+
+impl Parse for SecMarcoInput {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let var = syn::Ident::parse(input)?;
+        eprintln!("{:?}", var);
+        let _in = <Token![in]>::parse(input)?;
+        eprintln!("{:?}", _in);
+        let from = <LitInt>::parse(input)?;
+        let _dots = <Token![..]>::parse(input)?;
+        let to = <LitInt>::parse(input)?;
+        eprintln!("{:?}, {:?}, {:?}", from, _dots, to);
+        let block = <Block>::parse(input)?;
+        eprintln!("{:?}", block);
+
+        Ok(SecMarcoInput {})
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as SecMarcoInput);
+    println!("{:?}", ast);
 
-    unimplemented!()
+    TokenStream::new()
 }
